de7ca3fe4331bad9c7a42e13167c07cba7d1a8f6
Roop Singh <rsingh@ryanrts.com>
2024-12-07 00:24:14 -0600

    updating run_solver to a place to do analysis of parsed object

--- src/run_solver.py
+++ src/run_solver.py
@@ -1,6 +1,10 @@
 from eusolver import DCSolve
 from parsers.parser import SygusV2Parser
+from parsers.printer import SygusV2ASTPrinter
+from parsers.processor import SygusV2Processor
 import ply.yacc as yacc
+from pprint import pprint
+from parsers.ast import ASTVisitor
 
 def load_benchmark(file_path):
     parser = SygusV2Parser()
@@ -12,10 +16,14 @@ def load_benchmark(file_path):
 def benchmark_test():
     benchmark_file = './benchmarks/s1.sl'
 
-    components = load_benchmark(benchmark_file)
-    print(components)
+    program = load_benchmark(benchmark_file)
+    program_str = SygusV2ASTPrinter.run(program, None)
+    # print(program_str)
     # solver = DCSolve(components.term_grammar, components.predicate_grammar, components.spec)
-
+    for attr in dir(program):
+        print("obj.%s = %r" % (attr, getattr(program, attr)))
+    # each none empty line is a command, representated by a class in the ast file
+    print(program.commands)
     # result = solver.solve()
     # print("Result of the benchmark test:", result)
 
